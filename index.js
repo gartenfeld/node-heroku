d5d91d13b8cf891c6f65718a07a642db6bea50d9
David Rosson <david@rosson.com.au>
2015-08-12 10:23:19 -0700

    updated app settings

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+var publicDIR = path.join(__dirname, '/public');
+app.use(express.static(publicDIR));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.redirect('/index.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  // console.log('Node app is running on port', app.get('port'));
 });
 
 
